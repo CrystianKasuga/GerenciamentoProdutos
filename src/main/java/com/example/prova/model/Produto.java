2314c480d9b6f9a83c9f30dc67af0d83d877bb42
Crystian <crystiankenji04@gmail.com>
2024-07-02 20:30:57 -0300

    repository

--- src/main/java/com/example/prova/model/Produto.java
+++ src/main/java/com/example/prova/model/Produto.java
@@ -1,6 +1,8 @@
 package com.example.prova.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
@@ -9,6 +11,7 @@ import java.math.BigDecimal;
 public class Produto {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
